51f1c82000d35cf651cc0a21d06960bd16bfb0f4
Sir-Unkie <unknown920@gmail.com>
2021-07-03 05:52:45 +0300

    sticky nav

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
+//
+// DONT FORGET:
+// smooth scrolling
+// links to demo popups
+// navigation links
+// sticky nav
+// images quality
+// structured js file
+// popup opening on button clicks with js not css
+// Logo color
+// lazyloading
+//
+
 const myBtn = document.querySelector('#kkk');
 const back = document.querySelector('#popup');
 const popup = document.querySelector('.popup__content');
 const close = document.querySelector('.popup__close');
+const sectionAbout = document.querySelector('#section-about');
+const navigation = document.querySelector('.navigation');
+const header = document.querySelector('.header');
 
 myBtn.addEventListener('click', () => {
   back.classList.toggle('popup--visible');
@@ -17,3 +33,21 @@ back.addEventListener('click', e => {
     popup.classList.remove('popup__content--visible');
   }
 });
+
+// Sticky nav
+const obsCallback = entries => {
+  const [entry] = entries;
+  if (entry.isIntersecting === false) {
+    navigation.classList.add('navigation--sticky');
+  } else {
+    navigation.classList.remove('navigation--sticky');
+  }
+};
+
+const obsOptions = {
+  root: null,
+  threshold: 0.1,
+};
+
+const headerObserver = new IntersectionObserver(obsCallback, obsOptions);
+headerObserver.observe(header);
