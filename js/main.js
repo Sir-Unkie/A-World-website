cf757345bab165441feef945b60fa16a39eda794
Sir-Unkie <unknown920@gmail.com>
2021-07-04 05:19:38 +0300

    added another one popup and fixed some small poor visuals

--- js/main.js
+++ js/main.js
@@ -1,18 +1,14 @@
 //
-// DONT FORGET:
-// smooth scrolling
 // links to demo popups
-// navigation links
-// structured js file
-// Logo color
-// book now button inside of the popup
-// lazyloading
 //
 
-const myBtn = document.querySelector('#kkk');
-const back = document.querySelector('#popup');
-const popup = document.querySelector('.popup__content');
-const close = document.querySelector('.popup__close');
+const headerBtn = document.querySelector('#header-btn');
+const backDemoPopup = document.querySelector('#popup-demo');
+const back = document.querySelector('#popup-main');
+const popupMainContent = back.querySelector('.popup__content');
+const popupDemo = backDemoPopup.querySelector('.popup__content');
+const closeMainPopup = back.querySelector('.popup__close');
+const closeDemoPopup = backDemoPopup.querySelector('.popup__close');
 const sectionAbout = document.querySelector('#section-about');
 const navigation = document.querySelector('.navigation');
 const header = document.querySelector('.header');
@@ -20,32 +16,72 @@ const burger = document.querySelector('.burger');
 const mobileNav = document.querySelector('.navigation-mobile');
 const toursSection = document.querySelector('#section-tours');
 const navLogo = document.querySelector('.navigation__logo');
+const btnDummy = document.querySelector('#read-stories');
+const formSubmitBtn = document.querySelector('#form-submit');
+
+// demo popup handlers start
+
+formSubmitBtn.addEventListener('click', e => {
+  e.preventDefault();
+  console.log('asdasdasd');
+  backDemoPopup.classList.toggle('popup--visible');
+  popupDemo.classList.toggle('popup__content--visible');
+});
+
+btnDummy.addEventListener('click', e => {
+  e.preventDefault();
+  backDemoPopup.classList.toggle('popup--visible');
+  popupDemo.classList.toggle('popup__content--visible');
+});
+
+closeDemoPopup.addEventListener('click', () => {
+  backDemoPopup.classList.remove('popup--visible');
+  popupDemo.classList.remove('popup__content--visible');
+});
+
+backDemoPopup.addEventListener('click', e => {
+  if (e.target === backDemoPopup) {
+    backDemoPopup.classList.remove('popup--visible');
+    popupDemo.classList.remove('popup__content--visible');
+  }
+});
+
+// demo popup handlers end
 
 // popup handlers start ///////////////////////////////
 toursSection.addEventListener('click', e => {
   if (e.target.classList.value.includes('btn-popup')) {
     back.classList.toggle('popup--visible');
-    popup.classList.toggle('popup__content--visible');
+    popupMainContent.classList.toggle('popup__content--visible');
   }
 });
 
-myBtn.addEventListener('click', () => {
+headerBtn.addEventListener('click', () => {
   back.classList.toggle('popup--visible');
-  popup.classList.toggle('popup__content--visible');
+  popupMainContent.classList.toggle('popup__content--visible');
 });
 
-close.addEventListener('click', () => {
+closeMainPopup.addEventListener('click', () => {
   back.classList.remove('popup--visible');
-  popup.classList.remove('popup__content--visible');
+  popupMainContent.classList.remove('popup__content--visible');
 });
 
 back.addEventListener('click', e => {
   if (e.target === back) {
     back.classList.remove('popup--visible');
-    popup.classList.remove('popup__content--visible');
+    popupMainContent.classList.remove('popup__content--visible');
   }
 });
 // popup handlers end ///////////////////////////////
+
+// popup book button start ///////////////////////////////
+popupMainContent.addEventListener('click', e => {
+  if (e.target.classList.value.includes('btn')) {
+    back.classList.toggle('popup--visible');
+    popupMainContent.classList.toggle('popup__content--visible');
+  }
+});
+// popup book button end ///////////////////////////////
 
 // burger button handler start ///////////////////////////////
 burger.addEventListener('click', () => {
