6f17b008918b78344d65b487679acc13e1f95072
Sir-Unkie <unknown920@gmail.com>
2021-07-04 03:58:11 +0300

    improved logo and sticky nav

--- js/main.js
+++ js/main.js
@@ -3,11 +3,9 @@
 // smooth scrolling
 // links to demo popups
 // navigation links
-// sticky nav
-// images quality
 // structured js file
-// popup opening on button clicks with js not css
 // Logo color
+// book now button inside of the popup
 // lazyloading
 //
 
@@ -20,34 +18,52 @@ const navigation = document.querySelector('.navigation');
 const header = document.querySelector('.header');
 const burger = document.querySelector('.burger');
 const mobileNav = document.querySelector('.navigation-mobile');
+const toursSection = document.querySelector('#section-tours');
+const navLogo = document.querySelector('.navigation__logo');
 
-burger.addEventListener('click', () => {
-  mobileNav.classList.toggle('shown');
-  burger.classList.toggle('burger--close');
+// popup handlers start ///////////////////////////////
+toursSection.addEventListener('click', e => {
+  if (e.target.classList.value.includes('btn-popup')) {
+    back.classList.toggle('popup--visible');
+    popup.classList.toggle('popup__content--visible');
+  }
 });
 
 myBtn.addEventListener('click', () => {
   back.classList.toggle('popup--visible');
   popup.classList.toggle('popup__content--visible');
 });
+
 close.addEventListener('click', () => {
   back.classList.remove('popup--visible');
   popup.classList.remove('popup__content--visible');
 });
+
 back.addEventListener('click', e => {
   if (e.target === back) {
     back.classList.remove('popup--visible');
     popup.classList.remove('popup__content--visible');
   }
 });
+// popup handlers end ///////////////////////////////
 
-// Sticky nav
+// burger button handler start ///////////////////////////////
+burger.addEventListener('click', () => {
+  mobileNav.classList.toggle('shown');
+  burger.classList.toggle('burger--close');
+});
+
+// burger button handler end ///////////////////////////////
+
+// Sticky nav start ///////////////////////////////
 const obsCallback = entries => {
   const [entry] = entries;
   if (entry.isIntersecting === false) {
     navigation.classList.add('navigation--sticky');
+    navLogo.src = './assets/Logo/LogoLongBlack.png';
   } else {
     navigation.classList.remove('navigation--sticky');
+    navLogo.src = './assets/Logo/Logo1.png';
   }
 };
 
@@ -58,3 +74,4 @@ const obsOptions = {
 
 const headerObserver = new IntersectionObserver(obsCallback, obsOptions);
 headerObserver.observe(header);
+// sticky nav end ///////////////////////////////
